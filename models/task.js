09d4f82f8495b44daa27b7d39b3c40217e660071
Your Name <darshankgithub@gmail.com>
2025-11-28 20:12:44 +0530

    change the schema

--- models/task.js
+++ models/task.js
@@ -1,34 +1,44 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-        trim:true,
-    },
-    subject:{
-        type:String,
-        trim:true,
-        default:"General",
-    },
-    description:{
-        type:String,
-        trim:true,
-    },
-    dueDate:{
-        type:Date,
-    },
-    priority:{
-        type:String,
-        enum:["Low","Medium","High"],
-        default:"Low",
-    },
-    status:{
-        type:String,
-        enum:["pending","in progress","completed"],
-        default:"pending",
-    },
-})
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: v => !/^\d+$/.test(v)   // ❌ only numbers not allowed
+  },
 
-const Task = mongoose.model("Task",taskSchema)
+  subject: {
+    type: String,
+    trim: true,
+    default: "General",
+  },
+
+  description: {
+    type: String,
+    trim: true,
+    validate: v => !/^\d+$/.test(v)   // ❌ only numbers not allowed
+  },
+
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Low",
+  },
+
+  status: {
+    type: String,
+    enum: ["pending", "in progress", "completed"],
+    default: "pending",
+  },
+});
+
+const Task = mongoose.model("Task",taskSchema) 
 module.exports = Task
+
+
